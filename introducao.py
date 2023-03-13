bfaa57abf83127cba7e4430932b9e6b3978788e3
Isabelly Santos <isabelly.santos885@gmail.com>
2023-03-13 18:33:44 -0400

    Add files via upload

---
+++ introducao.py
@@ -0,0 +1,28 @@
+"""
+
+Linguagem de programação é um conjunto de regras, símbolos e convenções 
+usados para escrever programas de computador.
+
+Linguagem Natural: Idiomas, nas quais foram desenvolvidas naturalmente para fim de
+comunicação humana.
+
+Linguagem Formal: Criadas pelas pessoas para uma aplicação em específico. Notação
+matemática e fórmulas químicas são exemplos. Possuem um conjunto bem definido de:
+símbolos e palavras chaves, regras sintáticas e semânticas, como linguagens de
+programação
+
+Existem dois tipos principais de linguagens de programação: 
+baixo nível e alto nível.
+
+Linguagens de baixo nível são mais próximas da linguagem de máquina e são 
+usadas para programas que interagem diretamente com o hardware.
+
+Linguagens de alto nível são mais fáceis de entender e usar, permitindo que os 
+programadores se concentrem mais na lógica do programa do que nos detalhes 
+de baixo nível.
+
+Exemplos de linguagens de programação de alto nível incluem:
+Python, Java e JavaScript.
+Cada linguagem de programação tem suas próprias características e usos específicos.
+
+"""
