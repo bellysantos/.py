06b638bd95116f05d31c215ab348b2faef3a48d2
Isabelly Santos <isabelly.santos885@gmail.com>
2023-03-13 18:39:33 -0400

    Add files via upload

---
+++ conceitos.py
@@ -0,0 +1,33 @@
+"""
+
+Criada em 1991 por Guido van Rossum.
+O nome Python foi homenagem ao seu programa de humor favorito "Monty Python"
+
+Guido definiu em 1999 quais seriam os objetivos do Python:
+
+Intuita e Poderosa: Linguagem fácil e intuitiva, ao mesmo tempo poderosa.
+Open-source: Código aberto para contribuição
+Simples: Escrever um código deve ser tão simples quanto escrever texto em inglês.
+Versátil: Versátil para utilização desde tarefas simples até soluções completas.
+
+A evolução das versões do Python, resultam na atual (2008) que é a versão 3.0
+contando em consolidação uma das linguagens mais utilizadas no mundo. De acordo com
+a fonte TIOBE Index, ela é a linguagem mais popular do mundo.
+
+Ela é utilizada para tipos diferentes de soluções como: Ciência de dados, 
+aplicações web, jogos, inteligência artificial, banco de dados, entre outros.
+Também utilizada por muitas empresas como Netflix, Reddit, Instagram, Amazon,
+Pinterest, Spotify, entre outras. 
+
+Características: Python é uma linguagem de alto nível (fácil de usar e entender), 
+interpretada (código fonte é interpretado diretamente), de tipagem dinâmica (não é
+necessária a declaração dos tipos dos dados) e multiparadigma (procedural, 
+funcional e orientada a objetos).
+
+Vantagens: Fácil de aprender, Versatilidade e Flexibilidade, Prototipação e 
+Produtividade, Bibliotecas diversas, Open-source, Portabilidade.
+
+Desvantagens: Limitação de desempenho e alto consumo do memória, dispositivos
+móveis, problemas com paralelização.
+
+""" 
