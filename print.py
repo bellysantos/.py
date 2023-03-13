8291d47f9655a77c784a16a50dd2deba287fa743
Isabelly Santos <isabelly.santos885@gmail.com>
2023-03-13 18:42:45 -0400

    Add files via upload

---
+++ print.py
@@ -0,0 +1,12 @@
+nome = 'Isabelly'
+print('Olá', nome, end='!\n')
+
+coracao = 'Carlos'
+print('Olá', coracao, 'tudo bem?')
+
+numero1 = 1
+numero2 = 2
+print(numero1 + numero2) 
+
+amor = 'O Carlos ama a Isabelly <3'
+print(amor)
