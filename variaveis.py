7cf14be2cfc037d55052f976e3891792f2ee0ac8
Isabelly Santos <isabelly.santos885@gmail.com>
2023-03-14 21:36:12 -0300

    variaveis

---
+++ variaveis.py
@@ -0,0 +1,48 @@
+# Não é necessário declarar, o tipo, por exemplo: int valor.
+
+pessoa = 'Isabelly Santos'
+idade = 21
+altura = 1.55
+
+print(pessoa, idade, altura)
+
+texto = 'Este é um texto grande.'
+porcentagem = 0.5
+numero = -100
+
+print(texto, porcentagem, numero)
+
+diferenca_idade = 2023 - 2001
+print(idade)
+
+decimal = 15/4
+print(decimal)
+
+"""
+Regras para nomeação de variáveis:
+* Uma variável pode conter letras e números, mas não começar com número;
+* É recomendável uso de apenas letras minúsculas para nomes de variáveis;
+* Não é permitido caracteres especiais, espaço em branco e pontuação;
+* O único símbolo permitido é _ (underline);
+* Versões recentes do Python suportam letras acentuadas, mas é recomendável 
+	não fazer uso dete método.
+"""
+
+"""
+Palavras chaves:
+False;   None;    True;     and;       as;        assert;    async;
+await;   break;   class;    continue;  def;       del;       elif;
+else;    except;  finally;  for;       from;      global;    if;
+import;  in;      is;       lambda;    nonlocal;  not;       or;
+pass;    raise;   return;   try;       while;     with;      yield;
+"""
+
+variavel = 0
+Variavel2 = 99
+print(variavel, Variavel2) 
+
+# Lembrar que o Python é case-sensitive.
+nome = 'Isabelly'
+Nome = 'Carlos'
+
+print(nome, Nome)
